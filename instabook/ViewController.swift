7116cfef98b53272b7e39949350a14c0a75bbe9b
Stas Buldakov <xamelon96@gmail.com>
2018-12-06 15:53:45 +0400

    Add files

---
+++ instabook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instabook
+//
+//  Created by Stas Buldakov on 06/12/2018.
+//  Copyright © 2018 Stas Buldakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
